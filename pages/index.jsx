134b3d643fe4ada3a1e85e055e57da1e93d8949b
Diana1809 <diana.costache1809@gmail.com>
2022-11-07 15:34:45 +0200

    Monochrome Set-up FINAL

--- pages/index.jsx
+++ pages/index.jsx
@@ -3,8 +3,7 @@ import Image from 'next/image';
 export default function Home() {
   return (
     <div>
-      Hello
-      <img src="/images/Banner/banner.png" alt="Alt"></img>
+      <img src="/images/images/Banner/banner.png" alt="Alt"></img>
       <button className="button button-primary">Button Primary</button>
       <button className="button button-primary button-large">
         Button Primary Large
